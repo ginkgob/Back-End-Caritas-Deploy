6804a55b70b616b44e28ec8744de852356cb71fb
DamarisTeoc <teoc.918@gmail.com>
2022-06-21 17:17:48 +0200

    routes for get, update and delete user

--- src/routes/user.js
+++ src/routes/user.js
@@ -14,8 +14,40 @@ router.post('/users', (req, res) => {
 });
 
 
+// get all users
+router.get('/users', (req, res) => {
+    userSchema
+    .find()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
 
+// get a user
+router.get('/users/:id', (req, res) => {
+    const { id } = req.params;
+    userSchema
+    .findById(id)
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
 
+// update a user
+router.put('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const { name, age, email } = req.body;
+    userSchema
+    .updateOne({_id: id}, { $set: {name, age, email}})
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
 
+// delete a user
+router.delete('/users/:id', (req, res) => {
+    const { id } = req.params;
+    userSchema
+    .updateOne({_id: id},)
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
 
 module.exports = router;
