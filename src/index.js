ef31d75323765c4610b4bf38f892ac5724f166df
ginkgo <alexvid33@gmail.com>
2022-07-26 23:10:11 +0200

    set PORT variable

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import app from './app'
 import './database';
 
-const PORT = process.env.PORT || 5050
+const PORT = 5050
 
 
 
-const server = app.listen(PORT, () => {
+const server = app.listen(process.env.PORT || PORT, () => {
     console.log(`Server is running on ${PORT}`) 
 }) 
 
